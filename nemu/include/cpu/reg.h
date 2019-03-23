f769b195666dc0d0e2a34b6d1c6d5aa2e08a78f8
tracer-ics2015 <tracer@njuics.org>
2019-03-23 00:23:53 -0700

    >  compile NEMU 17307130038 caucher Linux ubuntu 4.15.0-47-generic #50-Ubuntu SMP Wed Mar 13 10:44:52 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  00:23:53 up  2:46,  1 user,  load average: 0.24, 0.09, 0.02 4feef9e090b20ecef7730e1080fb5ba71bdedbc6

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,7 +15,9 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
-	struct {
+union{
+
+	union {
 		uint32_t _32;
 		uint16_t _16;
 		uint8_t _8[2];
@@ -23,8 +25,19 @@ typedef struct {
 
 	/* Do NOT change the order of the GPRs' definitions. */
 
-	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
+	//uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	struct 
+	{
+		uint32_t eax;
+		uint32_t ecx;
+		uint32_t edx;
+		uint32_t ebx;
+		uint32_t esp;
+		uint32_t ebp;
+		uint32_t esi;
+		uint32_t edi;
+	};
+};	
 	swaddr_t eip;
 
 } CPU_state;
