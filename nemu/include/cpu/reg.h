5b77538e87c3fd007b08d96593ad24878786b941
tracer-ics2015 <tracer@njuics.org>
2019-03-25 04:08:54 -0700

    >  compile NEMU 17307130038 caucher Linux ubuntu 4.15.0-47-generic #50-Ubuntu SMP Wed Mar 13 10:44:52 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  04:08:54 up 3 min,  1 user,  load average: 0.66, 0.57, 0.25 c2371880751ee78c4772b87aa9ca8ef585ca7bde

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -29,13 +29,13 @@ union{
 	struct 
 	{
 		uint32_t eax;
-		uint32_t ecx;
 		uint32_t edx;
+		uint32_t ecx;
 		uint32_t ebx;
-		uint32_t esp;
 		uint32_t ebp;
 		uint32_t esi;
 		uint32_t edi;
+		uint32_t esp;
 	};
 };	
 	swaddr_t eip;
