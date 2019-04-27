f516b857a54bb393bb5f19358f3d893aa3272af7
tracer-ics2015 <tracer@njuics.org>
2019-04-27 06:24:45 -0700

    >  compile NEMU 17307130038 caucher Linux ubuntu 4.15.0-47-generic #50-Ubuntu SMP Wed Mar 13 10:44:52 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  06:24:45 up  3:01,  1 user,  load average: 0.17, 0.14, 0.07 12e758b4cad1fd77cd80a53c17dcc671b0371c0f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -165,12 +165,196 @@ static bool make_token(char *e) {
 	return true; 
 }
 
+uint32_t trans2num10(char * s)
+{
+	unsigned len=strlen(s);
+	unsigned i=0;
+	uint32_t ans=0;
+	for(;i<len;++i)
+		ans=ans*10+(s[i]-'0');
+	return ans;
+}
+
+uint32_t trans2num16(char *s)
+{
+	unsigned len=strlen(s);
+	unsigned i=0;
+	uint32_t ans=0;
+	for(;i<len;++i)
+		if(s[i]>='0'&&s[i]<='9')
+			ans=ans*16+(s[i]-'0');
+		else if(s[i]>='a'&&s[i]<='f')
+			ans=ans*16+(s[i]+10+s[i]-'a');
+		else if(s[i]>='A'&&s[i]<='F')
+			ans=ans*16+(s[i]+10+s[i]-'A');
+	return ans;	
+}
+/*
+uint32_t read_reg(char *s)
+{
+	int i;
+	if(strlen(s)==3)
+	{
+		for (i = 0; i < 8; ++i)
+			if(!strcmp(regsl[i],s))
+				return  cpu.gpr[i]._32;
+	}
+	else
+	{
+		for(i=0;i<8;++i)
+			if(!strcmp(regsw[i],s))
+				return (uint32_t)cpu.gpr[i]._16;
+		for(i=0;i<8;++i)
+			if(!strcmp(regsb[i],s))
+				return (uint32_t)cpu.gpr[i]._8[0];
+	}
+	
+}*/
+
+/*不仅查明表达式中括号是否匹配，还要看整个表达式是不是被一个括号括住*/
+bool check_parentheses(unsigned p,unsigned q)
+{
+	if(tokens[p].type!='('||tokens[q].type!=')')
+		return false;
+	unsigned counter=0;
+    unsigned i = p+1;
+    for (; i < q;++i)
+    {
+        if(tokens[i].type=='(')
+            ++counter;
+        else if(tokens[i].type==')')
+        {
+            if(!counter)
+			return false;
+			--counter;
+		}
+    }
+    if(!counter)
+        return true;
+    else
+        return false;
+}
+
+/*仅仅查明表达式中括号是否匹配*/
+bool check_only_parentheses(unsigned p,unsigned q)
+{
+	unsigned counter=0;
+    unsigned i = p;
+    for (; i <= q;++i)
+    {
+        if(tokens[i].type=='(')
+            ++counter;
+        else if(tokens[i].type==')')
+        {
+			if (!counter)
+				return false;
+			--counter;
+        }
+    }
+    if(!counter)
+        return true;
+    else
+        return false;
+}
+
+unsigned dominant_operator(unsigned p, unsigned q)
+{
+	unsigned i=p;
+	int candidate[32]={-1};
+	unsigned j;
+	for(j=0;j<32;++j)
+		candidate[j]=-1;
+	j=0;
+	for(;i<=q;++i)
+	{
+		if(tokens[i].type!='+'&&tokens[i].type!='-'&&tokens[i].type!='*'&&tokens[i].type!='/')
+			continue;
+
+		unsigned left=i-1,right=i+1;
+		bool flag=false;
+		while(1)
+		{
+			while(left>=p && tokens[left].type!='('){--left;}
+			while(right<=q && tokens[right].type!=')'){++right;}
+			if(left<p||right>q)	break;
+			flag=check_parentheses(left,right);
+			if(flag)	break;
+		}
+		if(flag)	continue;
+		candidate[j++]=i;
+	}
+	bool max_priority=0;
+	for(i=0;i<j;++i)
+		if(tokens[candidate[i]].type=='*'||tokens[candidate[i]].type=='/')
+			max_priority=1;
+	if(max_priority)
+	{
+		for(i=j-1;i>=0;--i)
+			if(tokens[candidate[i]].type=='+'||tokens[candidate[i]].type=='-')
+				return i;
+	}
+	else return j-1;
+}
+
+uint32_t eval(unsigned p,unsigned q)
+{
+	if(p > q) {
+        assert(0);
+    }
+    else if(p == q) { 
+		if(tokens[p].type==NUM)
+			return trans2num10(tokens[p].str);
+		else if(tokens[p].type==HEXNUM)
+			return trans2num16(tokens[p].str);
+		//else if(tokens[p].type==REGNAME)
+		//	return read_reg(tokens[p].str);
+		else
+			assert(0);
+		/* Single token.
+         * For now this token should be a number. 
+         * Return the value of the number.
+         */
+	}
+    else if(check_parentheses(p, q) == true) {
+        /* The expression is surrounded by a matched pair of parentheses. 
+         * If that is the case, just throw away the parentheses.
+         */
+        return eval(p + 1, q - 1); 
+    }
+    else {
+		assert(check_only_parentheses(p,q)==true);
+        unsigned op = dominant_operator(p,q);
+        uint32_t val1 = eval(p, op - 1);
+        uint32_t val2 = eval(op + 1, q);
+
+        switch(tokens[op].type) {
+            case '+': return val1 + val2;
+            case '-': return val1 - val2;
+            case '*': return val1 * val2;
+            case '/': return val1 / val2;
+			case EQUAL:return val1 == val2;
+			case NOTEQUAL:return val1 != val2;
+            default: assert(0);
+    	}
+	}
+}
+
 uint32_t expr(char *e, bool *success) {
 	if(!make_token(e)) {
 		*success = false;
 		return 0;
 	}
-
+	--nr_token;
+	*success=true;
+	int i;
+	for(i = 0; i < nr_token; i ++) 
+	{
+    if(tokens[i].type == '*' && (i == 0 || (tokens[i - 1].type !=NUM && tokens[i-1].type!=HEXNUM && tokens[i-1].type!=REGNAME && tokens[i-1].type!=')')) ) 
+        tokens[i].type = DEREF;
+	if(tokens[i].type == '-' && (i == 0 || (tokens[i - 1].type !=NUM && tokens[i-1].type!=HEXNUM && tokens[i-1].type!=REGNAME && tokens[i-1].type!=')')) ) 
+        tokens[i].type = NEG;
+	}
+	return eval(0,nr_token);
 	/* TODO: Insert codes to evaluate the expression. */
 	panic("please implement me");
 	return 0;
